eaac96aec1e67f0c42480c148eb91663ef697898
Shoyo Inokuchi <contact@shoyo.dev>
2020-10-29 13:20:25 +0900

    Add more detailed documentation for BufferManager module

--- src/buffer/buffer_manager.rs
+++ src/buffer/buffer_manager.rs
@@ -1,37 +1,66 @@
 use crate::storage::block::Block;
+use crate::storage::disk_manager::DiskManager;
 use std::collections::HashMap;
 
-/// The buffer manager is responsible for fetching/flushing blocks
-/// that are managed in memory.
+/// The buffer manager is responsible for fetching/flushing blocks that are
+/// managed in memory. Any blocks that don't exist in the buffer are retrieved
+/// from disk through the disk manager.
 pub struct BufferManager {
     /// Collection of buffer frames that can hold blocks
     buffer_pool: Vec<BufferFrame>,
     /// Mapping between blocks and the frames that hold them
     block_table: HashMap<u32, u32>,
+    /// Disk manager to access blocks on disk
+    disk_manager: DiskManager,
 }
 
 impl BufferManager {
-    pub fn new(size: u32) -> Self {
+    /// Construct a new buffer manager.
+    pub fn new(buffer_size: u32, disk_manager: DiskManager) -> Self {
         let mut pool = Vec::new();
-        for i in 0..size {
+        for i in 0..buffer_size {
             pool.push(BufferFrame::new(i))
         }
         Self {
             buffer_pool: pool,
             block_table: HashMap::new(),
+            disk_manager,
         }
     }
 
+    /// Return the size of the buffer pool.
     pub fn buffer_size(&self) -> usize {
         self.buffer_pool.len()
     }
 
+    /// Fetch the specified block from the buffer and return the block if
+    /// successful.
     pub fn fetch_block(&mut self, block_id: u32) -> Option<Block> {
         None
     }
 
+    /// Flush the specified block to disk.
     pub fn flush_block(&mut self, block_id: u32) -> Result<(), ()> {
-        Ok(())
+        Err(())
+    }
+
+    /// Pin the specified block to the buffer.
+    /// Pinned blocks will never be evicted. Concurrent threads must pin a
+    /// block to the buffer before operating on it.
+    pub fn pin_block(&self, block_id: u32) -> Result<(), ()> {
+        Err(())
+    }
+
+    /// Unpin the specified block from the buffer.
+    /// Unpinned blocks can be evicted. Concurrent threads must unpin a
+    /// block when finished operating on it.
+    pub fn unpin_block(&self, block_id: u32) -> Result<(), ()> {
+        Err(())
+    }
+
+    /// Allocate space for a new block and return the block if successful.
+    pub fn new_block(&self, block_id: u32) -> Option<Block> {
+        None
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,6 @@ fn main() {
     println!("minuSQL (2020)");
     println!("Enter .help for usage hints");
 
-    let bm = buffer::buffer_manager::BufferManager::new(128);
+    let dm = storage::disk_manager::DiskManager::new();
+    let bm = buffer::buffer_manager::BufferManager::new(128, dm);
 }
