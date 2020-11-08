8884e756af59bc7d3389a84766e5807242f8a2d7
Shoyo Inokuchi <contact@shoyo.dev>
2020-11-08 12:40:35 +0900

    Define relation heap methods

--- src/common/constants.rs
+++ src/common/constants.rs
@@ -1,6 +1,7 @@
-pub const DB_FILENAME: &str = "db.minusql";
+pub const DB_FILENAME: &str = "db.jin";
 pub const BLOCK_SIZE: u32 = 4096;
 
 /// Type aliases
 pub type BlockIdT = u32;
 pub type RelationIdT = u32;
+pub type RecordIdT = u32;
--- src/relation/heap.rs
+++ src/relation/heap.rs
@@ -1,5 +1,6 @@
 use crate::buffer::manager::BufferManager;
-use crate::common::constants::BlockIdT;
+use crate::common::constants::{BlockIdT, RecordIdT};
+use crate::relation::record::Record;
 
 /// A heap is a collection of blocks on disk which corresponds to a given relation.
 /// Blocks are chained together as a doubly linked list. Each block contains in its
@@ -13,10 +14,32 @@ pub struct Heap {
 }
 
 impl Heap {
+    /// Create a new heap for a database relation.
     pub fn new(head_block_id: BlockIdT, buffer_manager: BufferManager) -> Self {
         Self {
             head_block_id,
             buffer_manager,
         }
+    }
+
+    /// Insert a record into the relation.
+    pub fn insert(record: Record) -> Result<(), ()> {
+        Err(())
+    }
+
+    /// Mark the specified record as deleted.
+    /// The record is not actually deleted until .apply_delete() is called.
+    pub fn mark_delete(record_id: RecordIdT) -> Result<(), ()> {
+        Err(())
+    }
+
+    /// Apply a delete operation to the specified record.
+    pub fn apply_delete(record_id: RecordIdT) -> Result<(), ()> {
+        Err(())
+    }
+
+    /// Rollback a delete operation.
+    pub fn rollback_delete(record_id: RecordIdT) -> Result<(), ()> {
+        Err(())
     }
 }
