d8f2be721043a3151086afcefdf71a200becf322
Shoyo Inokuchi <contact@shoyo.dev>
2020-12-11 00:59:45 -0800

    Declare dummy integration tests

---
+++ tests/integration.rs
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2020.  Shoyo Inokuchi.
+ * Please refer to github.com/shoyo/jin for more information about this project and its license.
+ */
+
+use jin::buffer::manager::BufferManager;
+use jin::disk::manager::DiskManager;
+
+#[test]
+fn test_create_update_drop_table() {
+    assert!(false);
+}
+
+#[test]
+fn test_insert_update_delete_tuple() {
+    assert!(false);
+}
+
+#[test]
+fn test_create_index() {
+    assert!(false)
+}
