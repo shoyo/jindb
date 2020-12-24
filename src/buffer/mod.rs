7f3a5c4c2d98e32727e2b3480e9f1ab0b62d634b
Shoyo Inokuchi <contact@shoyo.dev>
2020-12-24 11:04:58 -0800

    Implement threadsafe buffer

--- src/buffer/mod.rs
+++ src/buffer/mod.rs
@@ -3,5 +3,40 @@
  * Please refer to github.com/shoyo/jin for more information about this project and its license.
  */
 
+use crate::common::{BufferFrameIdT, PageIdT};
+use crate::page::Page;
+use std::collections::{HashMap, LinkedList};
+use std::sync::{Arc, Mutex, RwLock};
+
 pub mod eviction_policies;
 pub mod manager;
+
+/// Type alias for a page protected by a R/W latch for concurrent access.
+pub type PageLatch = Arc<RwLock<Option<Page>>>;
+
+/// The database buffer and associated data structures.
+/// The buffer instance is managed by a buffer manager.
+pub struct Buffer {
+    pool: Vec<PageLatch>,
+    page_table: RwLock<HashMap<PageIdT, BufferFrameIdT>>,
+    free_list: Mutex<LinkedList<BufferFrameIdT>>,
+}
+
+impl Buffer {
+    pub fn new(size: BufferFrameIdT) -> Self {
+        let mut pool = Vec::with_capacity(size as usize);
+        let page_table = RwLock::new(HashMap::new());
+        let mut tmp_list = LinkedList::new();
+        for frame_id in 0..size {
+            pool.push(Arc::new(RwLock::new(None)));
+            tmp_list.push_back(frame_id);
+        }
+        let free_list = Mutex::new(tmp_list);
+
+        Self {
+            pool,
+            page_table,
+            free_list,
+        }
+    }
+}
