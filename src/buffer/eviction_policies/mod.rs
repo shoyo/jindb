8da8af691daa2fe1021ceca9c582fb1d1e75d661
Shoyo Inokuchi <contact@shoyo.dev>
2021-01-03 17:34:00 -0800

    Refactor eviction policy trait and add Slow variant

--- src/buffer/eviction_policies/mod.rs
+++ src/buffer/eviction_policies/mod.rs
@@ -5,6 +5,7 @@
 
 pub mod clock;
 pub mod lru;
+pub mod slow;
 
 use crate::common::BufferFrameIdT;
 
@@ -12,28 +13,26 @@ use crate::common::BufferFrameIdT;
 pub enum PolicyVariant {
     Clock,
     LRU,
+    Slow,
 }
 
 /// An eviction policy trait for the database buffer.
-/// The policy used decides which page in the buffer is evicted when the
-/// buffer is full and a new page is requested.
+/// The policy used decides which page in the buffer is evicted when the buffer is full and a
+/// new page is requested.
 ///
-/// As a general rule, pages that are pinned to the buffer are never
-/// evicted. This means that there are cases where no pages can be
-/// removed, and the eviction operation fails.
+/// As a general rule, pages that are pinned to the buffer are never evicted. This means that
+/// there are cases where no pages can be removed, and the eviction operation fails.
 
 pub trait EvictionPolicy {
-    /// Evict a page from the buffer according to the eviction policy and
-    /// return the corresponding frame ID.
-    fn evict(&mut self) -> Option<BufferFrameIdT>;
+    /// Evict a page from the buffer according to the eviction policy and return the
+    /// corresponding frame ID.
+    fn evict(&self) -> Option<BufferFrameIdT>;
 
-    /// Indicate that the specified frame contains a pinned page and should
-    /// not be evicted.
+    /// Indicate that the specified frame contains a pinned page and should not be evicted.
     /// Should be called after a page has been pinned to the buffer.
-    fn pin(&mut self, frame_id: BufferFrameIdT);
+    fn pin(&self, frame_id: BufferFrameIdT);
 
-    /// Indicate that the specified frame contains a page with a pin count
-    /// of zero and can not be evicted.
-    /// Should be called after a page reaches a pin count of zero.
-    fn unpin(&mut self, frame_id: BufferFrameIdT);
+    /// Indicate that the specified frame contains a page with a pin count of zero and can be
+    /// evicted. Should be called after a page reaches a pin count of zero.
+    fn unpin(&self, frame_id: BufferFrameIdT);
 }
