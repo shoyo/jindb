0613e1f6b9fd871672631d0687a66fe3d99a0860
Shoyo Inokuchi <contact@shoyo.dev>
2021-01-04 21:30:27 -0800

    Implement page create/fetch for buffer manager

--- src/buffer/manager.rs
+++ src/buffer/manager.rs
@@ -8,8 +8,9 @@ use crate::buffer::eviction_policies::lru::LRUPolicy;
 use crate::buffer::eviction_policies::slow::SlowPolicy;
 use crate::buffer::eviction_policies::{EvictionPolicy, PolicyVariant};
 use crate::buffer::{Buffer, PageLatch};
-use crate::common::{BufferFrameIdT, PageIdT};
+use crate::common::{BufferFrameIdT, PageIdT, DICTIONARY_PAGE_ID, PAGE_SIZE};
 use crate::disk::manager::DiskManager;
+use crate::page::classifier_page::ClassifierPage;
 use crate::page::dictionary_page::DictionaryPage;
 use crate::page::relation_page::RelationPage;
 use crate::page::PageVariant::Relation;
@@ -29,7 +30,7 @@ use std::sync::{Arc, Mutex};
 pub struct BufferManager {
     buffer: Buffer,
     disk_manager: DiskManager,
-    evict_policy: Arc<Mutex<Box<dyn EvictionPolicy + Send + Sync>>>,
+    evict_policy: Box<dyn EvictionPolicy + Send + Sync>,
 }
 
 impl BufferManager {
@@ -37,9 +38,9 @@ impl BufferManager {
     pub fn new(
         buffer_size: BufferFrameIdT,
         disk_manager: DiskManager,
-        evict_policy: PolicyVariant,
+        policy_variant: PolicyVariant,
     ) -> Self {
-        let policy: Box<dyn EvictionPolicy + Send + Sync> = match evict_policy {
+        let evict_policy: Box<dyn EvictionPolicy + Send + Sync> = match policy_variant {
             PolicyVariant::Clock => Box::new(ClockPolicy::new(buffer_size)),
             PolicyVariant::LRU => Box::new(LRUPolicy::new(buffer_size)),
             PolicyVariant::Slow => Box::new(SlowPolicy::new(buffer_size)),
@@ -48,51 +49,49 @@ impl BufferManager {
         Self {
             buffer: Buffer::new(buffer_size),
             disk_manager,
-            evict_policy: Arc::new(Mutex::new(policy)),
+            evict_policy,
         }
     }
 
+    /// Fetch necessary pages from disk to initialize in-memory data structures for page metadata.
+    pub fn initialize(&self) {
+        todo!()
+    }
+
     /// Initialize a relation page, pin it, and return its page latch.
-    pub fn create_relation_page(&self) -> Result<PageLatch, BufFrameErr> {
+    pub fn create_relation_page(&self) -> Result<PageLatch, NoBufFrameErr> {
         self._create_page(PageVariant::Relation)
     }
 
     /// Initialize a dictionary page, pin it, and return its page latch.
-    pub fn create_dictionary_page(&self) -> Result<PageLatch, BufFrameErr> {
+    pub fn create_dictionary_page(&self) -> Result<PageLatch, NoBufFrameErr> {
         self._create_page(PageVariant::Dictionary)
     }
 
     /// Initialize a new page, pin it, and return its page latch.
     /// If there are no open buffer frames and all existing pages are pinned, then return an error.
-    fn _create_page(&self, variant: PageVariant) -> Result<PageLatch, BufFrameErr> {
-        let policy = self.evict_policy.lock().unwrap();
-        let victim = policy.evict();
-        drop(policy); // Drop latch on eviction policy to minimize contention.
-
-        match victim {
+    fn _create_page(&self, variant: PageVariant) -> Result<PageLatch, NoBufFrameErr> {
+        match self.evict_policy.evict() {
             Some(frame_id) => {
                 // Acquire latches to specified page and page table.
-                let page_latch = self.buffer.pool[frame_id as usize].clone();
+                let page_latch = self._get_page_latch(frame_id);
                 let mut frame = page_latch.write().unwrap();
-                let mut page_table = self.buffer.page_table.write().unwrap();
 
                 // Flush the existing page out to disk if necessary.
                 if let Some(page) = frame.as_ref() {
-                    if page.is_dirty() {
-                        self.disk_manager
-                            .write_page(page.get_id(), page.get_data())
-                            .unwrap();
-                    }
+                    self._flush_page(page);
                 }
 
                 // Allocate space on disk and initialize the new page.
                 let page_id = self.disk_manager.allocate_page();
                 let mut new_page: Box<dyn Page + Send + Sync> = match variant {
+                    PageVariant::Classifier => Box::new(ClassifierPage::new()),
                     PageVariant::Dictionary => Box::new(DictionaryPage::new()),
                     PageVariant::Relation => Box::new(RelationPage::new(page_id)),
                 };
 
                 // Update the page table and pin the new page to the buffer.
+                let mut page_table = self.buffer.page_table.write().unwrap();
                 page_table.insert(new_page.get_id(), frame_id);
                 new_page.incr_pin_count();
                 *frame = Some(new_page);
@@ -100,15 +99,44 @@ impl BufferManager {
                 // Return a reference to the page latch.
                 Ok(page_latch.clone())
             }
-            None => Err(BufFrameErr::new()),
+            None => Err(NoBufFrameErr::new()),
         }
     }
 
     /// Fetch the specified page, pin it, and return its page latch.
     /// If the page does not exist in the buffer, then fetch the page from disk.
     /// If the page does not exist on disk, then return an error.
-    pub fn fetch_page(&self, page_id: PageIdT) -> Result<PageLatch, ()> {
-        Err(())
+    pub fn fetch_page(&self, page_id: PageIdT) -> Result<PageLatch, NoBufFrameErr> {
+        match self._page_table_lookup(page_id) {
+            // If the page is already in the buffer, pin it and return its latch.
+            Some(frame_id) => {
+                let page_latch = self._get_page_latch(frame_id);
+                let mut frame = page_latch.write().unwrap();
+                let mut page = frame.as_mut().unwrap();
+                page.incr_pin_count();
+                Ok(page_latch.clone())
+            }
+            // Otherwise, the page must be read from disk and replace an existing page in the
+            // buffer. If there aren't any pages that can be evicted, give up and return an error.
+            None => match self.evict_policy.evict() {
+                Some(frame_id) => {
+                    let page_latch = self._get_page_latch(frame_id);
+                    let frame = page_latch.write().unwrap();
+
+                    if let Some(page) = frame.as_ref() {
+                        self._flush_page(page);
+                    }
+
+                    let mut page_data = [0; PAGE_SIZE as usize];
+                    self.disk_manager.read_page(page_id, &mut page_data);
+
+                    let mut page_table = self.buffer.page_table.write().unwrap();
+
+                    Err(NoBufFrameErr::new())
+                }
+                None => Err(NoBufFrameErr::new()),
+            },
+        }
     }
 
     /// Delete the specified page.
@@ -117,20 +145,20 @@ impl BufferManager {
         Err(())
     }
 
-    /// Flush the specified page to disk.
-    pub fn flush_page(&self, _page_id: PageIdT) -> Result<(), ()> {
-        Err(())
+    /// Flush the specified page to disk. Do nothing if the page hasn't been modified.
+    ///
+    /// Note: This method in isolation is NOT thread-safe. This method should only be called in
+    /// the context of another buffer manager method which is thread-safe.
+    fn _flush_page(&self, page: &Box<dyn Page + Send + Sync>) {
+        if page.is_dirty() {
+            self.disk_manager
+                .write_page(page.get_id(), page.get_data())
+                .unwrap();
+        }
     }
 
     /// Flush all pages to disk.
     pub fn flush_all_pages(&self) -> Result<(), ()> {
-        Err(())
-    }
-
-    /// Pin the specified page to the buffer.
-    /// Pinned pages will never be evicted. Threads must pin a page to the
-    /// buffer before operating on it.
-    pub fn pin_page(&self, _page_id: PageIdT) -> Result<(), ()> {
         Err(())
     }
 
@@ -142,15 +170,19 @@ impl BufferManager {
     }
 
     /// Index the buffer pool and return the specified page latch.
-    fn _get_page_by_frame(&self, _frame_id: BufferFrameIdT) -> Result<PageLatch, ()> {
-        Err(())
+    fn _get_page_latch(&self, frame_id: BufferFrameIdT) -> PageLatch {
+        self.buffer.pool[frame_id as usize].clone()
     }
 
     /// Find the specified page in the page table, and return its frame ID.
     /// If the page does not exist in the page table, then return None.
     /// Panic if the frame ID is out-of-bounds.
-    fn _page_table_lookup(&self, _page_id: PageIdT) -> Option<BufferFrameIdT> {
-        None
+    fn _page_table_lookup(&self, page_id: PageIdT) -> Option<BufferFrameIdT> {
+        let page_table = self.buffer.page_table.read().unwrap();
+        match page_table.get(&page_id) {
+            Some(frame_id) => Some(*frame_id),
+            None => None,
+        }
     }
 }
 
@@ -159,11 +191,11 @@ impl BufferManager {
 /// Error to be thrown when no buffer frames are open, and every page occupying a buffer frame is
 /// pinned and cannot be evicted.
 #[derive(Debug)]
-pub struct BufFrameErr {
+pub struct NoBufFrameErr {
     msg: String,
 }
 
-impl BufFrameErr {
+impl NoBufFrameErr {
     fn new() -> Self {
         Self {
             msg: format!("No available buffer frames, and all pages are pinned"),
