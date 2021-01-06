10179825ed06e36dc6014af823c24059fd317848
Shoyo Inokuchi <contact@shoyo.dev>
2021-01-06 14:09:48 -0800

    Extract write-mode file open functionality as utility function

--- src/disk/mod.rs
+++ src/disk/mod.rs
@@ -3,4 +3,17 @@
  * Please refer to github.com/shoyo/jin for more information about this project and its license.
  */
 
+use std::fs::{File, OpenOptions};
+
 pub mod manager;
+
+/// Utility functions for disk management.
+
+/// Open a file in write-mode.
+fn open_write_file(filename: &str) -> File {
+    OpenOptions::new()
+        .create(true)
+        .write(true)
+        .open(filename)
+        .unwrap()
+}
