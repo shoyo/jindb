1db18c1dd880a634e34805f65c5dad6e2894d08e
Shoyo Inokuchi <contact@shoyo.dev>
2021-02-01 11:38:24 -0800

    Slightly optimize slow unpin() implementation with a hashset

--- src/buffer/replacement/slow.rs
+++ src/buffer/replacement/slow.rs
@@ -5,7 +5,7 @@
 
 use crate::buffer::replacement::PageReplacer;
 use crate::constants::BufferFrameIdT;
-use std::collections::VecDeque;
+use std::collections::{HashSet, VecDeque};
 use std::sync::{Arc, Mutex};
 
 /// A terribly inefficient eviction policy with O(1) evict and O(N) pin/unpin operations. This
@@ -13,16 +13,20 @@ use std::sync::{Arc, Mutex};
 /// Use a LRU or clock based eviction policy during actual database use.
 pub struct SlowReplacer {
     queue: Arc<Mutex<VecDeque<BufferFrameIdT>>>,
+    set: Arc<Mutex<HashSet<BufferFrameIdT>>>,
 }
 
 impl SlowReplacer {
     pub fn new(buffer_size: BufferFrameIdT) -> Self {
         let mut queue = VecDeque::with_capacity(buffer_size as usize);
+        let mut set = HashSet::with_capacity(buffer_size as usize);
         for frame_id in 0..buffer_size {
             queue.push_back(frame_id);
+            set.insert(frame_id);
         }
         Self {
             queue: Arc::new(Mutex::new(queue)),
+            set: Arc::new(Mutex::new(set)),
         }
     }
 }
@@ -30,52 +34,55 @@ impl SlowReplacer {
 impl PageReplacer for SlowReplacer {
     fn evict(&self) -> Option<u32> {
         let mut queue = self.queue.lock().unwrap();
-        queue.pop_front()
+        let mut set = self.set.lock().unwrap();
+
+        match queue.pop_front() {
+            Some(frame_id) => {
+                assert!(set.remove(&frame_id));
+                Some(frame_id)
+            }
+            None => None,
+        }
     }
 
     fn pin(&self, frame_id: u32) {
         let mut queue = self.queue.lock().unwrap();
+        let mut set = self.set.lock().unwrap();
 
-        let mut count = 0;
-        let mut idx = 0;
-
-        for i in 0..queue.len() {
-            if queue[i] == frame_id {
-                count += 1;
-                idx = i;
+        // If `frame_id` has already been evicted or pinned, it does not exist in the set and and
+        // the following operation is a no-op.
+        // If `frame_id` exists in the set, it is removed from both the set and queue.
+        if set.remove(&frame_id) {
+            let matches = queue
+                .iter()
+                .enumerate()
+                .filter(|(i, &id)| id == frame_id)
+                .collect::<Vec<(usize, &BufferFrameIdT)>>();
+            match matches.len() {
+                0 => panic!("Frame ID {} exists in the set but not the queue"),
+                1 => {
+                    let idx = matches[0].0;
+                    queue.remove(idx);
+                }
+                _ => panic!(
+                    "Found {} instances of frame ID {} in queue, expected 0 or 1",
+                    matches.len(),
+                    frame_id
+                ),
             }
         }
-
-        if count > 1 {
-            panic!(
-                "Found {} instances of frame ID {} in queue, expected 0 or 1",
-                count, frame_id
-            );
-        }
-        if count == 1 {
-            queue.remove(idx);
-        }
-        // If the page has already been pinned, count will equal 0 and we do nothing.
     }
 
     fn unpin(&self, frame_id: u32) {
         let mut queue = self.queue.lock().unwrap();
+        let mut set = self.set.lock().unwrap();
 
-        let count = queue
-            .iter()
-            .filter(|id| **id == frame_id)
-            .collect::<Vec<&u32>>()
-            .len();
-        if count > 1 {
-            panic!(
-                "Found {} instance(s) of frame ID {} in queue, expected 0",
-                count, frame_id
-            );
-        }
-        if count == 0 {
+        // If `frame_id` did not exist in the set, then it is inserted into both the set and queue.
+        // If `frame_id` already existed in the set, then set.insert() returns false and the
+        // following operation is a no-op.
+        if set.insert(frame_id) {
             queue.push_back(frame_id);
         }
-        // If the page has already been unpinned, count will equal 1 and we do nothing.
     }
 }
 
